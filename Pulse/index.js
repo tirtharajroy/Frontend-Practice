d5d57d1f9e49884693dada4daf0d4f237c44a53d
Tirtharaj Roy <tirtharajroy6@gmail.com>
2023-06-12 21:30:25 +0530

    Completed Pulse

--- Pulse/index.js
+++ Pulse/index.js
@@ -6,9 +6,11 @@ toggleBtn.addEventListener('click', function () {
         toggleBtn.classList.remove('fa-bars');
         toggleBtn.classList.add('fa-times');
         navUl.style.display = 'block';
+        document.querySelector('body').style.overflow = 'hidden';
     } else {
         toggleBtn.classList.remove('fa-times');
         toggleBtn.classList.add('fa-bars');
         navUl.style.display = '';
+        document.querySelector('body').style.overflow = 'auto';
     }
 });
