f648427bc94a679f17d725914ccc443ef4d98429
Tirtharaj Roy <tirtharajroy6@gmail.com>
2023-06-14 00:24:09 +0530

    Completed Tracks Section in Monstercat

--- Monstercat/index.js
+++ Monstercat/index.js
@@ -1,1 +1,39 @@
-// Function For Audio Player
+// Audio Player Function
+
+let audioActive = {
+    element: null,
+    audio: null
+}
+
+function playAudio(element, audioPath) {
+    let player = element;
+
+    
+    if (element.classList.contains('fa-play')) {
+        if (audioActive.element && audioActive.element !== element) {
+            audioActive.element.classList.remove('fa-pause');
+            audioActive.element.classList.add('fa-play');
+            audioActive.element.style.paddingRight = '';
+            audioActive.audio.pause();
+        }
+
+        player.classList.remove('fa-play');
+        player.classList.add('fa-pause');
+        player.style.paddingRight = '0.155rem';
+        
+        if (!audioActive.audio || audioActive.element !== element) {
+            audioActive.audio = new Audio(audioPath);
+        }
+        audioActive.audio.play();
+        audioActive.element = element;
+
+    } else {
+        player.classList.remove('fa-pause');
+        player.classList.add('fa-play');
+        player.style.paddingRight = '';
+
+        if (audioActive.audio) {
+            audioActive.audio.pause();
+        }
+    }
+}
